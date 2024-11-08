77296c2c7ac8a422a4df938c45d2bb9342f807b1
percyz30 <percynoeyarihuaman@gmail.com>
2024-11-08 14:08:11 -0500

    feat:actualizo reflex
    actualizando mas detalles de mi landing page

--- TASK_ORGANIZER_LandingPage/componentes/encabezado.py
+++ TASK_ORGANIZER_LandingPage/componentes/encabezado.py
@@ -4,12 +4,8 @@ def encabezado()->rx.Component:
   return rx.hstack(
       rx.hstack(
         rx.icon(tag="notebook-pen"),
-        rx.heading("Task Organizer",size="6",weight="bold"),
+        rx.heading("Task Organizer...",size="6",weight="bold"),
         align_items="center"
-      ),
-      rx.hstack(
-        rx.link("Acerca de ",href="/#",size="3",weight="bold"),
-        justify="end",
       ),
       justify="between",
       align_items="center",
--- TASK_ORGANIZER_LandingPage/componentes/seccion.py
+++ TASK_ORGANIZER_LandingPage/componentes/seccion.py
@@ -4,11 +4,19 @@ def seccion()->rx.Component:
   return rx.vstack(
       rx.heading(
         rx.text.span("T.Organizer",color=rx.color("jade",10)),
-        " te ayudara a organizarte mejor"),
+        " te ayudara a organizarte mejor",size="9"),
 
       rx.container(
-        rx.text("planifica, organiza y sigue tus tareas en una plataforma intuitiva que se adapta a tus necesidades. Desde recordatorios hasta integración con tus herramientas favoritas, ¡es todo lo que necesitas para ser más eficiente! ",margin_top="4em"),
-        rx.link(rx.button("Registrate",margin_top="6em"),href="https://forms.gle/qg1UsWrgkbh15K5y9",is_external=True),
+        rx.hstack(
+          rx.text("Planifica, organiza y sigue tus tareas en una plataforma intuitiva que se adapta a tus necesidades. Desde recordatorios hasta integración con tus herramientas favoritas, ¡es todo lo que necesitas para ser más eficiente! ",margin_top="4em",weight="bold",
+                size="5"),
+          rx.image(src="",alt="mi imagen")
+        ),
+        rx.hstack(
+          rx.link(rx.button("Registrate",margin_top="6em",size="4"),href="https://forms.gle/mVBJT5NJPs2Xwy8j6",is_external=True),
+            justify="end",
+            margin_top="4em"
+        )
       ),
       padding_top="6em",
       align="center",
